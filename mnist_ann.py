a319ddd2ed66d45b3ee381ccd1519a9ffba766f1
Param Popat <param.popat@gmail.com>
2018-08-08 09:36:53 +0530

    Updated MNIST ANN

--- mnist_ann.py
+++ mnist_ann.py
@@ -5,12 +5,10 @@ __git__ = "https://github.com/parampopat/"
 """
 
 
-import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.datasets import mnist
 from keras.utils import *
-from sklearn.metrics import accuracy_score
 
 
 def get_model(xtrain, ytrain, xtest, ytest):
@@ -32,21 +30,20 @@ def train():
     Main function to train the model
     :return:
     """
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    X_train = X_train.reshape(60000, 784)
-    print(X_train.shape[1])
-    X_test = X_test.reshape(10000, 784)
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    X_train /= 255
-    X_test /= 255
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(60000, 784)
+    print(x_train.shape[1])
+    x_test = x_test.reshape(10000, 784)
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
     digits = 10
-
     y_train = to_categorical(y_train, digits)
     y_test = to_categorical(y_test, digits)
-    model = get_model(X_train, y_train, X_test, y_test)
-    y_test_pred = model.predict(X_test)
-    acc = model.evaluate(X_test, y_test, verbose=0)
+    model = get_model(x_train, y_train, x_test, y_test)
+    y_test_pred = model.predict(x_test)
+    acc = model.evaluate(x_test, y_test, verbose=0)
     print('Test Acc:', acc[1])
     return model
 
